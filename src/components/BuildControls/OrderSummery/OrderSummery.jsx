b52af647b8f5ffb7c7be16c2f389e8dbeb6fe37e
behrang1360 <behrang1360@gmail.com>
2019-12-25 19:08:48 +0100

    Part one

--- src/components/BuildControls/OrderSummery/OrderSummery.jsx
+++ src/components/BuildControls/OrderSummery/OrderSummery.jsx
@@ -1,32 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import Wrapper from "../../../hoc/Warpper";
 import Button from "../../UI/Button/Button";
 
-const orderSummery = porps => {
-  let ingerdientsSummery = Object.keys(porps.ingerdients).map(key => {
+class OrderSummery extends Component {
+
+  componentWillUpdate() { 
+
+  }
+
+  render() {
+    let ingerdientsSummery = Object.keys(this.props.ingerdients).map(key => {
+      return (
+        <li key={key}>
+          {" "}
+          {key} : {this.props.ingerdients[key]}{" "}
+        </li>
+      );
+    });
+
     return (
-      <li key={key}>
-        {" "}
-        {key} : {porps.ingerdients[key]}{" "}
-      </li>
+      <Wrapper>
+        <h3>Your order</h3>
+        <ul>{ingerdientsSummery}</ul>
+        <p>
+          <strong>Total Price : {this.props.price.toFixed(2)}</strong>
+        </p>
+        <Button Clicked={this.props.cancelClicked} style="Danger">
+          Cancel
+        </Button>
+        <Button style="Success" Clicked={this.props.continueClicked}>
+          Continue
+        </Button>
+      </Wrapper>
     );
-  });
+  }
+}
 
-  return (
-    <Wrapper>
-      <h3>Your order</h3>
-      <ul>{ingerdientsSummery}</ul>
-      <p>
-        <strong>Total Price : {porps.price.toFixed(2)}</strong>
-      </p>
-      <Button Clicked={porps.cancelClicked} style="Danger">
-        Cancel
-      </Button>
-      <Button style="Success" Clicked={porps.continueClicked}>
-        Continue
-      </Button>
-    </Wrapper>
-  );
-};
-
-export default orderSummery;
+export default OrderSummery;
--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -1,23 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import Warpper from "../../../hoc/Warpper";
 import "./Modal.css";
 import Backdrop from "../BackDrop/BackDrop";
 
+class modal extends Component {
 
-const modal = props => (
-  <Warpper>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div
-      className="Modal"
-      style={{
-        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-        opacity: props.show ? "1" : "0"
-      }}
-    >
-      {props.children}
-    </div>
-
-  </Warpper>
-);
+  componentWillUpdate(prevState, nextState) { 
+    return (prevState.show != nextState.show)
+    
+  }
+  render() {
+    return (
+      <Warpper>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className="Modal"
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0"
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Warpper>
+    );
+  }
+}
 
 export default modal;
